cae3b90eb57fe4b9ef547ae6b435166798f07a3b
Gustavo Almeida <gustavo.almei2@hotmail.com>
2023-02-01 17:47:34 -0300

    adicionando faq

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,24 @@
 document.addEventListener("DOMContentLoaded", function() {
+    tabsMovies();
+    faq();
+
+})
+
+function faq(){
+    const questions = document.querySelectorAll("[data-faq-answer]");
+    const modifier = 'faq__questions__item--is-open';
+
+    for (let i = 0; i < questions.length; i++) {
+        questions[i].addEventListener("click", function(element){
+            const currentElement = element.target.parentNode;
+            currentElement.classList.toggle(modifier)
+        })
+    }
+}
+
+function tabsMovies(){
     const buttons = document.querySelectorAll("[data-tab-button]");
+
     for(let i = 0; i < buttons.length; i++) {
         buttons[i].addEventListener("click", function(btn){
             const abas = btn.target.dataset.tabButton
@@ -10,7 +29,7 @@ document.addEventListener("DOMContentLoaded", function() {
             btn.target.classList.add("shows__tabs__button-is--active")
         })
     }
-})
+}
 
 function removeIndex(){
     const buttons = document.querySelectorAll("[data-tab-button]");
